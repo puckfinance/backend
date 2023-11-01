229332954b293cfeb70fd4a4edbc038840dbcb68
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2023-11-01 19:43:46 +0800

    Fix build config

--- push-docker-hub.sh
+++ push-docker-hub.sh
@@ -1,2 +1,3 @@
-docker build --progress=plain -t nyamkamunhjin/binance-bot-v2 .
+docker build --platform=linux/amd64 --progress=plain -t nyamkamunhjin/binance-bot-v2 .
+
 docker push nyamkamunhjin/binance-bot-v2:latest
